7da2d8a32da5ede27dfdc81bc5ac3a72dd9ec3a6
Gaby <gaby.es.iaa@gmail.com>
2022-02-28 16:09:29 -0500

    Create Categories componet

---
+++ src/components/Categories.js
@@ -0,0 +1,9 @@
+const Category = () => (
+  <main className="main">
+    <section>
+      <h2 className="section__title">Category page under construction</h2>
+    </section>
+  </main>
+);
+
+export default Category;
