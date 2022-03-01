c1f07852e6b588b43e606b262f59c9ac598dec6a
Gaby <gaby.es.iaa@gmail.com>
2022-02-28 19:08:25 -0500

    Update the Home component, deleting the booksArray static array, and the prop it pass to BookList component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,9 @@
 import BookInput from './BookInput';
 import BookList from './BookList';
 
-const booksArray = [
-  {
-    id: '1',
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    category: 'Action',
-  },
-  {
-    id: '2',
-    title: 'Dune',
-    author: 'Frank Herbert',
-    category: 'Science Fiction',
-  },
-  {
-    id: '3',
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    category: 'Economy',
-  },
-];
-
 const Home = () => (
   <main className="main">
-    <BookList booksArray={booksArray} />
+    <BookList />
     <BookInput />
   </main>
 );
